e22c6a061c50f053612b74c8c4ba9d20bc16778f
David Obodo <obododavid5@gmail.com>
2020-01-31 03:25:46 +0100

    redirecting user on login successful

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import Login from './Login';
 import SignUp from './SignUp';
 import './auth.css';
 
 const Auth = () => {
 
+    const { firebase } = useSelector(state => {
+        return {
+            firebase: state.firebase
+        }
+    })
+
+    if (firebase.auth.uid) {
+        return <Redirect to="/" />
+    }
     return (
         <div className='auth__wrapper'>
             <Login style={{ border: '3px solid red' }} />
