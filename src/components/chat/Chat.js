67c6a69b4af5f7795e8718fbe767920f4c5bf802
David Obodo <obododavid5@gmail.com>
2020-02-22 15:07:18 +0100

    moment outputting date properly

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -2,7 +2,6 @@ import React from 'react';
 import moment from 'moment';
 
 const Chat = ({ message, clicked, senderId, userId, time }) => {
-    console.log(time)
     let is_user_msg;
 
     if (senderId == userId) {
@@ -11,14 +10,12 @@ const Chat = ({ message, clicked, senderId, userId, time }) => {
         is_user_msg = false;
     }
 
-    // className={`Chat ${is_user_msg ? "is-user-msg" : ""}`}
-
     return (
         <span className={`Chat__wrapper ${is_user_msg ? "is-user-msg" : ""}`} onClick={clicked}>
             <span className={`Chat__message ${is_user_msg ? "is-user-msg" : ""}`} >
                 {message}
             </span>
-            <span className="Chat__time">20:22pm</span>
+            <span className="Chat__time">{moment(time.toDate()).calendar()}</span>
         </span>
     )
 };
