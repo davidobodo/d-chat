f904a7f487731e427c6dc83f31316be777ba7ca2
David Obodo <obododavid5@gmail.com>
2020-03-01 15:28:51 +0100

    create env file

--- src/fbConfig.js
+++ src/fbConfig.js
@@ -3,14 +3,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAwuajOuBV1dZQOnDUVziiQwjwhBCfXndc",
-    authDomain: "d-chat-98abe.firebaseapp.com",
-    databaseURL: "https://d-chat-98abe.firebaseio.com",
-    projectId: "d-chat-98abe",
-    storageBucket: "d-chat-98abe.appspot.com",
-    messagingSenderId: "441517185856",
-    appId: "1:441517185856:web:27deb6a5591c7403bd78a5",
-    measurementId: "G-TH6YGMVDT4"
+    apiKey: process.env.D_CHAT_apiKey,
+    authDomain: process.env.D_CHAT_authDomain,
+    databaseURL: process.env.D_CHAT_databaseURL,
+    projectId: process.env.D_CHAT_projectId,
+    storageBucket: process.env.D_CHAT_storageBucket,
+    messagingSenderId: process.env.D_CHAT_messagingSenderId,
+    appId: process.env.D_CHAT_appId,
+    measurementId: process.env.D_CHAT_measurementId,
 };
 
 firebase.initializeApp(firebaseConfig);
