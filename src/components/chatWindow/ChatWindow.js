05a4ac172de02c2e0888336a382b111e3ea41e36
David Obodo <obododavid5@gmail.com>
2020-07-05 00:45:17 +0100

    refactor

--- src/components/chatWindow/ChatWindow.js
+++ src/components/chatWindow/ChatWindow.js
@@ -1,39 +1,41 @@
 import React from "react";
-import { useSelector, shallowEqual } from "react-redux";
-import Header from '../header/Header'
-import Chats from '../chats/Chats'
-import store from '../../store/store'
-import './ChatWindow.css'
-import MessageInput from '../messageInput/MessageInput';
-import { firestoreConnect } from 'react-redux-firebase';
+import PropTypes from "prop-types";
+import {useSelector, shallowEqual} from "react-redux";
+import Header from "../header/Header";
+import Chats from "../chats/Chats";
+import store from "../../store/store";
+import "./ChatWindow.css";
+import MessageInput from "../messageInput/MessageInput";
+import {firestoreConnect} from "react-redux-firebase";
 
-const ChatWindow = ({ activeChatUserId, userId }) => {
-	const { allUsers, firestoreMessages } = useSelector(state => ({
-		allUsers: state.firestore.ordered.users,
-		allMessages: state.messages,
-		firestoreMessages: state.firestore.ordered.messages
-	}), shallowEqual)
+const ChatWindow = (props) => {
+	const {activeChatUserId, userId} = props;
+	const {allUsers, firestoreMessages} = useSelector(
+		(state) => ({
+			allUsers: state.firestore.ordered.users,
+			allMessages: state.messages,
+			firestoreMessages: state.firestore.ordered.messages,
+		}),
+		shallowEqual
+	);
 
-	const activeChat = allUsers.filter(user => user.id === activeChatUserId);
+	const activeChat = allUsers.filter((user) => user.id === activeChatUserId);
 
-	const { typing } = store.getState()
-
-
+	const {typing} = store.getState();
 
 	return (
 		<div className="ChatWindow">
 			<Header activeChat={activeChat} activeChatUserId={activeChatUserId} />
-			<Chats
-				messages={firestoreMessages}
-				userId={userId}
-				activeChatUserId={activeChatUserId} />
+			<Chats messages={firestoreMessages} userId={userId} activeChatUserId={activeChatUserId} />
 			<MessageInput value={typing} activeChatUserId={activeChatUserId} userId={userId} />
 		</div>
 	);
 };
 // export default firestoreConnect(() => ['users', 'messages'])(ChatWindow);
 
-export default firestoreConnect([
-	{ collection: 'users' },
-	{ collection: 'messages', orderBy: ['time', 'desc'] }
-])(ChatWindow);
+ChatWindow.propTypes = {
+	activeChatUserId: PropTypes.any,
+	userId: PropTypes.any,
+};
+
+export default firestoreConnect([{collection: "users"}, {collection: "messages", orderBy: ["time", "desc"]}])(ChatWindow);
