b2e898b123a1786501ef5e3451a160633adf26f0
David Obodo <obododavid5@gmail.com>
2020-07-05 00:33:28 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
-import Auth from './pages/auth/auth';
-import Home from './pages/home/Home';
+import Auth from "./pages/auth/auth";
+import Home from "./pages/home/Home";
 
 const App = () => {
-
-
 	return (
 		<Router>
 			<Switch>
@@ -14,7 +12,7 @@ const App = () => {
 				<Route exact path="/auth" component={Auth} />
 			</Switch>
 		</Router>
-	)
-}
+	);
+};
 
 export default App;
