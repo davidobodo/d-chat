f8196ab4c2a712dd6061894131fa56704d95c668
David Obodo <obododavid5@gmail.com>
2020-07-05 00:46:13 +0100

    refactor

--- src/components/chatWindow/ChatWindow.js
+++ src/components/chatWindow/ChatWindow.js
@@ -1,9 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {
-	useSelector,
-	shallowEqual,
-} from "react-redux";
+import {useSelector, shallowEqual} from "react-redux";
 import Header from "../header/Header";
 import Chats from "../chats/Chats";
 import store from "../../store/store";
@@ -13,48 +10,24 @@ import {firestoreConnect} from "react-redux-firebase";
 
 const ChatWindow = (props) => {
 	const {activeChatUserId, userId} = props;
-	const {
-		allUsers,
-		firestoreMessages,
-	} = useSelector(
+	const {allUsers, firestoreMessages} = useSelector(
 		(state) => ({
-			allUsers:
-				state.firestore.ordered.users,
+			allUsers: state.firestore.ordered.users,
 			allMessages: state.messages,
-			firestoreMessages:
-				state.firestore.ordered.messages,
+			firestoreMessages: state.firestore.ordered.messages,
 		}),
 		shallowEqual
 	);
 
-	const activeChat = allUsers.filter(
-		(user) => user.id === activeChatUserId
-	);
+	const activeChat = allUsers.filter((user) => user.id === activeChatUserId);
 
 	const {typing} = store.getState();
 
 	return (
 		<div className="ChatWindow">
-			<Header
-				activeChat={activeChat}
-				activeChatUserId={
-					activeChatUserId
-				}
-			/>
-			<Chats
-				messages={firestoreMessages}
-				userId={userId}
-				activeChatUserId={
-					activeChatUserId
-				}
-			/>
-			<MessageInput
-				value={typing}
-				activeChatUserId={
-					activeChatUserId
-				}
-				userId={userId}
-			/>
+			<Header activeChat={activeChat} activeChatUserId={activeChatUserId} />
+			<Chats messages={firestoreMessages} userId={userId} activeChatUserId={activeChatUserId} />
+			<MessageInput value={typing} activeChatUserId={activeChatUserId} userId={userId} />
 		</div>
 	);
 };
